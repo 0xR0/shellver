5b58d5a3d5e41108abc0a53ae640d8bce700eb83
0xR <42720080+0xR0@users.noreply.github.com>
2018-11-24 02:19:39 +0100

    Update shellver.py

--- shellver.py
+++ shellver.py
@@ -16,7 +16,7 @@ def resize():
 def shell():
 	color = ['\033[95m' , '\033[96m', '\033[36m' , '\033[94m' , '\033[92m' , '\033[93m' , '\033[91m']
 	lan = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
-	wan = get('https://ipapi.co/ip/').text
+	wan = get('https://api.ipify.org/').text
 	print choice(color) + "For lan enter 1:",(lan)
 	print choice(color) + "For wan enter 2:",(wan)
 	cw = raw_input("Which one do you want lan or wan : ")
